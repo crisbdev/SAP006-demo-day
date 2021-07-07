143f44ca1f032c7f7cd0b9603ba40842e567f536
RAFAELA <72530170+RafaelaCugini@users.noreply.github.com>
2021-07-07 16:53:40 -0300

    feat: personagem1

--- src/main.js
+++ src/main.js
@@ -1,6 +1,26 @@
 import { example } from './data.js';
-// import data from './data/lol/lol.js';
-import data from './data/pokemon/pokemon.js';
-// import data from './data/rickandmorty/rickandmorty.js';
 
-console.log(example, data);
+import data from './data/rickandmorty/rickandmorty.js';
+
+console.log(data.results[0]);
+
+const cards = document.getElementById("cards")
+console.log(cards);
+
+let personagemUm = data.results[0];
+
+function createcard (personagem) {
+
+  const div = `
+    <div>
+      <img src="${personagem.image}">
+    </div>
+  `
+  return div;
+}
+
+let result = createcard(personagemUm);
+
+console.log(result);
+
+cards.innerHTML = result
