88d025628720b7a8c8b2ca825a138ef405b69de7
Carla Nogueira <keyseprojectcontato@gmail.com>
2021-07-21 16:26:31 -0300

    Filtros-funcionando

--- src/data.js
+++ src/data.js
@@ -1,15 +1,36 @@
-export const filtrarPerson = (personsInfo, selectOption) => {
-  const arrayFiltrado = personsInfo.filter(function (personagem) {
-    //console.log(personagem.status);
-    //console.log(personagem.status === selectOption);
-    if (personagem.status === selectOption) {
-      return personagem
-    }
-  })
-  return arrayFiltrado
+export const filterStatus = (personsInfo, selectOption, type) => {
+    const arrayFiltrado = personsInfo.filter(function (personagem) {
+        //console.log(personagem.status);
+        //console.log(personagem.status === selectOption);
+        if (personagem[type] === selectOption) {
+            return personagem
+        }
+    })
+    return arrayFiltrado
     //console.log(arrayFiltrado);
 };
 
-export const anotherExample = () => {
-  
-};
+/*export const filterSpecie = (personsInfo, selectOptionSpecie) => {
+    const arrayFiltradoSpecie = personsInfo.filter(function (personagem) {
+        //console.log(personagem.species);
+        //console.log(personagem.species === selectOptionSpecie);
+        if (personagem.species === selectOptionSpecie) {
+            return personagem
+        }
+    })
+    return arrayFiltradoSpecie
+ 
+};    
+
+//Definir uma function para chamar dentro da function da linha 16?
+export const filterGender = (personsInfo, selectOptionGender) => {
+    const arrayFiltradoGender = personsInfo.filter(function (personagem) {
+        //console.log(personagem.gender);
+        //console.log(personagem.gender === selectOptionGender);
+        if (personagem.gender === selectOptionGender) {
+            return personagem
+        }
+    })
+    return arrayFiltradoGender
+
+};*/
